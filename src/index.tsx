29ae655cf5fc36b2f971a4509d6a251b1d512eca
EunBin Hyun <dmsqlsgus@naver.com>
2023-02-10 21:44:26 +0900

    ✨ Register service worker

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,19 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register({
+  onUpdate: (register: ServiceWorkerRegistration) => {
+    console.log('onUpdate');    
+    navigator.serviceWorker.getRegistrations().then(regs =>
+      regs.forEach(reg => {
+        reg?.waiting?.postMessage({ type: 'SKIP_WAITING' });
+      })
+    );
+  },
+  onSuccess: (register: ServiceWorkerRegistration) => {
+    console.log('onSuccess');
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
