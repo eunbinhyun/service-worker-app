e73ed84ad158f61dd897d7d88244cf5cd0ecd896
EunBin Hyun <dmsqlsgus@naver.com>
2023-02-11 10:29:12 +0900

    🐛 Edit place context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { UpdateAlarmProvider } from "./context/UpdateAlarmContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <UpdateAlarmProvider>
+      <App />
+    </UpdateAlarmProvider>
   </React.StrictMode>
 );
 
@@ -19,13 +21,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-serviceWorkerRegistration.register({
-  onUpdate: () => {
-    console.log('onUpdate');
-    // context?.handleShowUpdate(true);
-  },
-  onSuccess: () => {
-    console.log('onSuccess')
-  }
-});
