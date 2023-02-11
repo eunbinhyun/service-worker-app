719ddf8521a09e12cd7def78e831cc0d3fea2baf
EunBin Hyun <dmsqlsgus@naver.com>
2023-02-11 10:55:43 +0900

    🔨edit place serviceworker register

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,21 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Banner } from './components/Banner/Banner';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 export const AppWrapper = ({children}: {children: JSX.Element}) => {
   const [showUpdate, setShowUpdate] = useState(false);
 
   useEffect(() => {
-    serviceWorkerRegistration.register({
-      onUpdate: () => {
-        console.log("onUpdate");
-        setShowUpdate(true);
-      },
-      onSuccess: () => {
-        console.log("onSuccess");
-      },
-    });
-
+    
     navigator.serviceWorker.getRegistrations().then((regs) =>
       regs.forEach((reg) => {
         if (reg.waiting) setShowUpdate(true);
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppWrapper } from './AppWrapper';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -21,3 +22,13 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register({
+  onUpdate: () => {
+    console.log("onUpdate");
+//    setShowUpdate(true);
+  },
+  onSuccess: () => {
+    console.log("onSuccess");
+  },
+});
